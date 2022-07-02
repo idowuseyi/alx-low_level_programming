d895ecce95c80ed0cae4f5cd9385576c81aeb68d
idowuseyi <idowuseyi22@gmail.com>
2022-07-02 12:59:43 -0700

    betty guidelines

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 /**
- *
- * main - prints to string
- *
- *
- *
- * Description: Prints "and that piece of art is useful..." without puts
- *
- *
- *
- * Return: 1
- *
- */
+* main - prints to string
+* Description: Prints "and that piece of art is useful..." without puts
+* Return: 1
+*/
 
 int main(void)
+/* main function declaration */
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	long l = 59;
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall" 
+	__asm__ ("syscall"
 			: "=a" (ret)
 			: "a" (syscall),
 			"D" (fd),
