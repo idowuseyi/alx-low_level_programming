733b02484270d69ebd6b30c3850ccbfa02661e3c
idowuseyi <idowuseyi22@gmail.com>
2022-07-01 17:47:58 -0700

    correcting printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main (void)
-{print("with proper grammar, but the outcome is a piece of art, \n");
+{printf("with proper grammar, but the outcome is a piece of art, \n");
 
 	Return(0);
 }
