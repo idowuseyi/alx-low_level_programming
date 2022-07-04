880c55093a84118a2f15b86b10aa6873ee83cd7c
idowuseyi <idowuseyi22@gmail.com>
2022-07-03 21:55:14 -0700

    3-print_alphabets.c betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,31 +1,25 @@
-#include <stdio.h>
-
-
-
-/*
+/**
+ * main - entry point
  *
- * main - prints in lower case
+ * Desription: "prints the alphabets in the lower case and then in uppercase"
  *
- * Description: "prints the alphabet in lowercase, and then in uppercase"
- *
- * Return: 0
+ * Return: alway return 0
  *
  */
-
 int main(void)
 {
-	char e = 'a';
-	char f = 'A';
-	while (e <= 'z')
-	{
-	putchar(e);
-	e++;
-	}
-	while (f <= 'Z')
-	{
-	putchar(f);
-	f++;
-	}
-	putchar('\n');
-	return (0);
+char e = 'a';
+char f = 'A';
+while (e <= 'z')
+{
+putchar(e);
+e++;
+}
+while (f <= 'Z')
+{
+putchar(f);
+f++;
+}
+putchar('\n');
+return (0);
 }
