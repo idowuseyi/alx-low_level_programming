24b52d58594194781b0508a496aa6351be68f2fb
idowuseyi <idowuseyi22@gmail.com>
2022-07-04 09:37:33 -0700

    still learning betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<unistd.h>
 /**
-* main - prints to string
-* Description: Prints "and that piece of art is useful..." without puts
-* Return: 1
+* main - Prints out a specifi statement as output
+*
+* Return: 1 if success
 */
 
 int main(void)
-/* main function declaration */
 {
-	char s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	long l = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
-	__asm__ ("syscall"
-
-			: "=a" (ret)
-			: "a" (syscall),
-			"D" (fd),
-			"S" (s),
-			"d" (l));
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+Return(1)
 }
+
