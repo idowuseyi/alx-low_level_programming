0bc2b6e478edfec2d249b8133c0c1b54894ef222
idowuseyi <idowuseyi22@gmail.com>
2022-07-05 17:53:09 -0700

    putchar2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,11 @@
 #include "main.h"
 /**
-* chapt - print _putchar
+* main - call function
 *
-* Return: void
+* Return: always 0 (success)
 */
-
-void chapt(void)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	chapt();
+	return (0);
 }
