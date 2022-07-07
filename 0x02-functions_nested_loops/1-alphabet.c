c9d1c3f45bf33a01ac50793bf401b98c3fa15967
idowuseyi <idowuseyi22@gmail.com>
2022-07-06 21:47:57 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,16 @@
 #include "main.h"
+/**
+* main - print alphabets
+*
+* Return: always 0 (success)
+*/
+void print_alphabet(void)
+{
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
