c4a899caf3dfeeeee6b46e8c75a8c94feab8839e
idowuseyi <idowuseyi22@gmail.com>
2022-07-07 00:51:19 -0700

    updating islower with betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* _islower check if an alphabet is lower case
-*
-* Return: always 1 if c is lowercase and 0 otherwise
+* _islower-> check if an alphabet is lower case
+* @c: character to be checked on
+* Return: returns 1 if c is lowercase and 0 otherwise
 */
 int _islower(int c)
+/** c is an argument of casentest */
 {
 	return (c >= 'a' && c <= 'z');
 }
