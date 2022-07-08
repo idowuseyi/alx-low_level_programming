a18b29d257f1aa930876c9fc4857828128dafb5a
idowuseyi <idowuseyi22@gmail.com>
2022-07-08 09:25:10 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h
+
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	int c;
+
+	for (c = 'A' && c = 'Z' c++)
+	{
+		_putchar('c');
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+## ALX LOW LEVEL PROGRAMMING
+
+# This direcotry contains More C - functions, more nested loops
+
+Task 0 -isupper:
+Write a function that checks for uppercase character.
