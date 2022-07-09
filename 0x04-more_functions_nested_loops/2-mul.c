64720e7dc333d9c99b31bac39c94a3fe1444aee3
idowuseyi <idowuseyi22@gmail.com>
2022-07-08 21:17:05 -0700

    updating function multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,14 +2,14 @@
 
 /**
 * mul-> multiplies two numbers and give the output
-* @a,b is the numbers to be multiplied
+*@a:imultiplicity
+* @b: is the numbers to be multiplied
 * Return: return the output mul
 */
 
 int mul(int a, int b)
 {
-	int mul = a*b;
+	int mul = a * b;
 
 	return (mul);
-
 }
