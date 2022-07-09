b801e1973026a869e69d34b1bfa3d31509937d86
idowuseyi <idowuseyi22@gmail.com>
2022-07-08 20:56:43 -0700

    updating function print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,8 +9,7 @@ void print_number(int n)
 {
 	long m; /* power of 10 */
 	int c; /* boolean check */
-	long num; /* convert int to
-		     long */
+	long num; /* convert int to long */
 
 	num = n;
 	/* negatives */
