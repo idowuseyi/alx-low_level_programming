f459f6e3c9694de9e7447a677338be3029733da1
idowuseyi <idowuseyi22@gmail.com>
2022-07-12 16:32:51 -0700

    updating swap

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* reset_to_98-> function that places pointers
+*@p: is the variable to be used
+* Return: return *p
+*/
+int reset_to_98(int *p)
+{
+	*p = 98;
+
+	return (*p);
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int-> swap integer values
+* @a: the first pointer
+* @b: is the second pointer
+* Return: void
+*/
+
+int swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+}
