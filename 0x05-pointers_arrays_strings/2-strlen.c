2f318f85e1c47a8d211aff737528d59af5c0f436
idowuseyi <idowuseyi22@gmail.com>
2022-07-12 14:56:30 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen-> print out the the lenght of a string
+* @s: string to be counted
+* Return: string lenght
+*/
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
