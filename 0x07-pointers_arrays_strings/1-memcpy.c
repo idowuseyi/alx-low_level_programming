b9c1414412b20f1ea7c018b6fbcad0a894033200
jonero82 <jonero82@gmail.com>
2022-07-18 04:10:20 -0700

    even more functions

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _memcpy -> function copies n bytes from memory area src to memory area dest
+* @dest: memory destination to be copied into
+* @src: memory area to be copied from
+* @n: number of bytes to be copied from memory area
+* Return: returns a pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	memcpy(dest, src, n);
+	return (dest);
+
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strchr -> function that locates a character in a string.
+* @s: first string
+* @c: char to be checked for occurrence in string s
+* Return: returns a pointer to the first occurrence of the xter c in string s
+* or NULL if the character is not found
+*
+*/
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (s);
+
+}
