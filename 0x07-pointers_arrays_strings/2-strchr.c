abc07315c9c4bafbc9967dbc37965dc7d7201e6a
idowuseyi <idowuseyi22@gmail.com>
2022-07-18 10:40:48 -0700

    strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,18 +5,21 @@
 * @c: char to be checked for occurrence in string s
 * Return: returns a pointer to the first occurrence of the xter c in string s
 * or NULL if the character is not found
-*
 */
 char *_strchr(char *s, char c)
 {
 	int a = 0, b;
 
 	while (s[a])
+	{
 		a++;
-	for (b = 0; b < a; b++)
+	}
+	for (b = 0; b <= a; b++)
 	{
 		if (c == s[b])
+		{
 			s += b;
+		}
 		return (s);
 
 	}
