62f301e693ee27063e8917da17c1bd8bd9faf3b3
idowuseyi <idowuseyi22@gmail.com>
2022-07-19 00:13:31 -0700

    more files

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,6 +8,16 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	strpbrk(s, accept);
-	return (s);
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+
+	return (NULL);
 }
