6c5280886aea638ec69a0e4e4f15664c6cd2a484
idowuseyi <idowuseyi22@gmail.com>
2022-07-20 15:49:15 -0700

    master of recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* factorial -> recursive function that calculate factorial
+* @n: n factorial to be calculated
+* Return: an integer
+*/
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
