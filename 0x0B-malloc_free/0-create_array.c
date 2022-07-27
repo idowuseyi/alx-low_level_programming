c3d142f44ad73a4e8fd9a5eb24b29fb1dd00e946
idowuseyi <idowuseyi22@gmail.com>
2022-07-26 17:08:08 -0700

    hot fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,5 +7,16 @@
   */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
 
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 char **strtow(char *str);
 char *argstostr(int ac, char **av);
