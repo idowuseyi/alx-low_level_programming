5249a71eff91c5ba8f02f617d743509efa4fdc98
idowuseyi <idowuseyi22@gmail.com>
2022-07-28 20:16:54 -0700

    hot fix string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	for (index = 0; s1[index]; index++)
 		len++;
-	concat = (char *)malloc(sizeof(char) + len + 1);
+	concat = (char *)malloc(sizeof(char) * len + 1);
 	if (concat == NULL)
 	return (NULL);
 	len = 0;
-	for (index = 0;: s1[index]; index++)
+	for (index = 0; s1[index]; index++)
 		concat[len++] = s1[index];
 	for (index = 0; s2[index] && index < n; index++)
 		concat[len++] = s2[index];
