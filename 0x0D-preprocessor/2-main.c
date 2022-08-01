d544df70901ceff4c2a506b2790ac795551ab45c
idowuseyi <idowuseyi22@gmail.com>
2022-08-01 13:06:50 -0700

    finish it

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print the source file name
+* Return: Return 0 (siccess)
+*/
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
