7ea633a04b22ad41edf72009c6853d9b463a1d72
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2022-08-02 02:05:47 +0100

    definining do struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog{char *name; float age; char *owner};
+/**
+* struct dog -> definition of dog structure.
+* @name: name of the dog
+* @age: the age of the dog
+* @owner: the owner name of the dog
+* Return: return void
+*/
+struct dog
+
+{
+char *name;
+float age;
+char *owner};
+};
 
 #endif
