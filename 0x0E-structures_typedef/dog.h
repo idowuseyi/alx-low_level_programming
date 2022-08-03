effbafc020aa7820a83e31005967c50fa41d8c66
idowuseyi <idowuseyi22@gmail.com>
2022-08-03 08:21:06 -0700

    taking effect

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,23 +9,19 @@
 * @owner: the owner name of the dog
 * Return: return void
 */
-struct dog
+typedef struct dog
 
 {
 char *name;
 float age;
 char *owner;
-};
-
-typedef struct dog_t
-{
-char *name;
-float age;
-char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #include <stdio.h>
 #include <stdlib.h>
