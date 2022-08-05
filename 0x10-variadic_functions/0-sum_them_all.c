e23873fdb5e84da289a5e87138ddcc49e40345ce
idowuseyi <idowuseyi22@gmail.com>
2022-08-04 20:03:54 -0700

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ptr;
 
 	va_start(ptr, n);
-	if (n == 0)
-	{
-	return (0);
-	}
+/*
+*	if (n == 0)
+*	{
+*	return (0);
+*	}
+*/
 	for (i = 0; i < n; i++)
 	{
 	sum += va_arg(ptr, int);
