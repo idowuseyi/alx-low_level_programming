8034b2456286d2af659814ba364a2aad9a83c712
idowuseyi <idowuseyi22@gmail.com>
2022-08-18 21:18:46 -0700

    crackme

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <limits.h>
-
+#define MAX_COUNT (sizeof(unsigned long int) * 8)
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
