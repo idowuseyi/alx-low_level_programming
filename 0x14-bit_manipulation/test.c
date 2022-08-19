7cc44884efbca5a6e9e941cf7731f1b7044d9269
idowuseyi <idowuseyi22@gmail.com>
2022-08-19 14:59:48 -0700

    cracking me

---
+++ 0x14-bit_manipulation/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int ibrahim(int a, int b)
+{
+	return(a+b);
+}
+
+int main(void)
+{
+	//int sum = 0;
+	//int Num1 = 5;
+	//int Num2 = 8;
+	//sum = Num1+Num2;
+	printf("%d\t%d\t%d\t%d\n", ibrahim(5,8),ibrahim(4,6), ibrahim(3, 7), ibrahim(3, 5));
+}
