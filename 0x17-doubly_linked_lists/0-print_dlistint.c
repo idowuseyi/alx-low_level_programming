dca62437e8031bf87ae93d813c1e12c88a916f79
idowuseyi <idowuseyi22@gmail.com>
2022-09-30 19:47:48 -0700

    adding new files

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,11 +6,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-int count = 0;
+size_t count = 0;
 while (h != NULL)
 {
 	printf("%d\n", h->n);
-	count += 1;
+	count++;
 	h = h->next;
 }
 return (count);
