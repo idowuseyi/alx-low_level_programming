30f03d94179d992a03580aa456dd0051612114a8
idowuseyi <idowuseyi22@gmail.com>
2022-10-03 16:28:47 -0700

    make me win more

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/idowuseyi/alx-low_level_programming/blob/50ced285c5c1e40cd9758c521e71a5120777a77e/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
