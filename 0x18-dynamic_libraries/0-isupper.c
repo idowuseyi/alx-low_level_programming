08e71a6442610a6b8a4e41e26915f5c12afd5177
idowuseyi <idowuseyi22@gmail.com>
2022-10-03 02:20:11 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isupper-> check if an alphabet is lower case
+* @c: character to be checked on
+* Return: returns 1 if c is lowercase and 0 otherwise
+*/
+int _isupper(int c)
+	/** c is an argument of casentest */
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memcpy -> function copies n bytes from memory area src to memory area dest
+* @dest: memory destination to be copied into
+* @src: memory area to be copied from
+* @n: number of bytes to be copied from memory area
+* Return: returns a pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+
+}
